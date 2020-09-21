37fc80519ef2b0c0decb57571d5a1e8c20c83e16
Mikael Josias <mikaeljosias2002@gmail.com>
2020-09-21 16:58:36 -0300

    3.Rendering Todos

--- App_de_Todos/todos.js
+++ App_de_Todos/todos.js
@@ -8,3 +8,16 @@ var todos = [
     'Estuda JS',
     'Acessar Comunidade da RocketSeat'
 ];
+
+function renderTodos(){
+    for(todo of todos){
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        todoElement.appendChild(todoText);
+
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos();
