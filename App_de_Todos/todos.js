35d46a47dc577614bcb5947faf0225049673fd74
Mikael Josias <mikaeljosias2002@gmail.com>
2020-09-21 16:37:34 -0300

    2.Iniatilising Application

--- App_de_Todos/todos.js
+++ App_de_Todos/todos.js
@@ -1,1 +1,10 @@
-alert('Funcionou');
+var listElement = document.querySelector('#app ul');
+var buttonElement = document.querySelector('#app button');
+var inputElement = document.querySelector('#app input');
+
+
+var todos = [
+    'Fazer Café',
+    'Estuda JS',
+    'Acessar Comunidade da RocketSeat'
+];
