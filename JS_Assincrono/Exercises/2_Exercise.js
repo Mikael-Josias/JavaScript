f8547f1cf9b4b1f3df526a56e8776002434e8333
Mikael Josias <mikaeljosias2002@gmail.com>
2020-09-22 17:11:07 -0300

    3rd Exercise

--- JS_Assincrono/Exercises/2_Exercise.js
+++ JS_Assincrono/Exercises/2_Exercise.js
@@ -9,13 +9,14 @@ var ulElement = document.getElementById('uList');
 function SearchUserRepos(){
     var urlGitUser = urlGitApi + inpUser.value + reposGitUser;
 
+    loadingReposList();
     axios.get(urlGitUser)
     .then(function(response){
         //console.log(response);
         createReposList(response.data);
     })
     .catch(function(error){
-        alert(error);
+        errorRequest(error.response.status);
     });
 }
 
@@ -29,6 +30,39 @@ function createReposList(repos){
         liElement.appendChild(textElement);
         ulElement.appendChild(liElement);
     }
+
+    divElement.appendChild(ulElement);
+}
+
+function errorRequest(code = 0){
+    ulElement.innerHTML = '';
+    
+    var liElement = document.createElement('li');
+    var textElement;
+
+    switch (code) {
+        case 404:
+            textElement = document.createTextNode('Usuário não existe!');
+            break;
+        default:
+            textElement = document.createTextNode('Erro na Requisição!');
+            break;
+    }
+
+    liElement.appendChild(textElement);
+    ulElement.appendChild(liElement);
+
+    divElement.appendChild(ulElement);
+}
+
+function loadingReposList(){
+    ulElement.innerHTML = '';
+    
+    var liElement = document.createElement('li');
+    var textElement = document.createTextNode('Carregando...');
+
+    liElement.appendChild(textElement);
+    ulElement.appendChild(liElement);
 
     divElement.appendChild(ulElement);
 }
