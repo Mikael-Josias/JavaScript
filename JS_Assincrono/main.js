035108e134998b4de75c1d78ab44371249cd7047
Mikael Josias <mikaeljosias2002@gmail.com>
2020-09-22 13:45:45 -0300

    1.AJAX Requests

---
+++ JS_Assincrono/main.js
@@ -0,0 +1,11 @@
+var xhr = new XMLHttpRequest();
+
+xhr.open('get', 'https://api.github.com/users/Mikael-Josias');
+xhr.send(null);
+
+
+xhr.onreadystatechange = function(){
+    if (xhr.readyState == 4) {
+        console.log(JSON.parse(xhr.responseText));
+    }
+}
